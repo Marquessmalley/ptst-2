f028faf90e1c0062c38f62a04625bcfee6ea4d4d
Marques Smalley <marquessmalley@gmail.com>
2025-04-29 15:20:42 -0400

    Fixed responsive layout

--- components/booking/SelectPackage.tsx
+++ components/booking/SelectPackage.tsx
@@ -5,7 +5,7 @@ const SelectPackage = () => {
   console.log(detailMenu);
   return (
     <div>
-      <div className="grid grid-cols-2 sm:grid-cols-3 ">
+      <div className="grid grid-cols-2 sm:grid-cols-3 gap-y-4 gap-x-2">
         {detailMenu.map((x, index) => (
           <div key={index}>
             <PackageCard />
--- components/ui/banner/DisclaimerBanner.tsx
+++ components/ui/banner/DisclaimerBanner.tsx
@@ -1,9 +1,9 @@
 const DisclaimerBanner = () => {
   return (
-    <div className="py-6 flex justify-center">
-      <div className="flex w-fit justify-center items-center rounded-2xl bg-white px-3 py-2 sm:px-2 sm:py-1">
-        <div className="flex flex-wrap items-center gap-x-4 gap-y-2">
-          <p className=" text-xs sm:text-sm/6 font-bold text-gray-900 whitespace-nowrap">
+    <div className="py-6 flex justify-center m-2">
+      <div className="flex w-fit justify-center items-center rounded-2xl bg-white pt-2 sm:px-2 sm:py-1">
+        <div className="flex flex-wrap items-center">
+          <p className=" text-xs sm:text-sm/6 font-bold text-gray-900 text-center ">
             🔔 Estimated price — subject to change after inspection or service
             adjustments.
           </p>
