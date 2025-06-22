45f33dc15e0b233758020f0872103a04937c6129
Marques Smalley <marquessmalley@gmail.com>
2025-06-22 19:34:40 -0400

    Added event type logic

--- app/api/resend/sendEmail/route.ts
+++ app/api/resend/sendEmail/route.ts
@@ -2,19 +2,28 @@ import { resend } from "@/lib/api/resend";
 import EmailTemplate from "@/components/booking/EmailTemplate";
 
 export async function POST(request: Request) {
+  const body = await request.json();
+
   try {
-    const { data, error } = await resend.emails.send({
-      from: "marquessmalley@gmail.com",
-      to: ["ksmalley77@gmail.com"],
-      subject: "Paul & Tev Shine Time Confirmation",
-      react: EmailTemplate(),
-    });
+    if (body?.type === "booking.created") {
+      const booking = body.data?.object?.booking;
+      const email = booking?.customer_details?.email_address;
 
-    if (error) {
-      return Response.json({ error }, { status: 500 });
+      console.log("📅 Booking received:", booking);
+
+      const { data, error } = await resend.emails.send({
+        from: "marquessmalley@gmail.com",
+        to: ["ksmalley77@gmail.com"],
+        subject: "Paul & Tev Shine Time Confirmation",
+        react: EmailTemplate(),
+      });
+
+      if (error) {
+        return Response.json({ error }, { status: 500 });
+      }
+
+      return Response.json(data);
     }
-
-    return Response.json(data);
   } catch (err) {
     console.log(err);
   }
