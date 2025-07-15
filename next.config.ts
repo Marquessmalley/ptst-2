09c35c4849a17a5c201b0f923c6a0fd3c42a50eb
msmalley-bizstream <msmalley@bizstream.com>
2025-07-15 09:58:48 -0400

    Added bundle analyzer

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,22 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "heroui.com",
-        port: "",
-        pathname: "/images/**",
-        search: "",
+        protocol: 'https',
+        hostname: 'heroui.com',
+        port: '',
+        pathname: '/images/**',
+        search: '',
       },
     ],
   },
 };
 
-export default nextConfig;
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+
+module.exports = withBundleAnalyzer(nextConfig);
