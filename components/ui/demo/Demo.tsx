6ab6808105ee5830a9acca65ca96d88e19cb459e
msmalley-bizstream <msmalley@bizstream.com>
2025-07-17 16:14:28 -0400

    Added new demo video

--- components/ui/demo/Demo.tsx
+++ components/ui/demo/Demo.tsx
@@ -59,8 +59,8 @@ const Demo = () => {
                 leaveTo="opacity-0 scale-95"
               >
                 <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white text-left align-middle shadow-xl transition-all">
-                  <video controls width="600">
-                    <source src="/Video-1.mp4" type="video/mp4" />
+                  <video controls width="600" className="h-2/5 w-full">
+                    <source src="/ptDemo.mp4" type="video/mp4" />
                     Your browser does not support the video tag.
                   </video>
                 </Dialog.Panel>
