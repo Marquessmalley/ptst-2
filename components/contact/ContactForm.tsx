9c8a3228c38f4ee506f7bf017ce94b2f2059b6ff
msmalley-bizstream <msmalley@bizstream.com>
2025-07-17 11:53:42 -0400

    more contact form progress

--- components/contact/ContactForm.tsx
+++ components/contact/ContactForm.tsx
@@ -17,7 +17,7 @@ const ContactForm = () => {
               name="first-name"
               type="text"
               autoComplete="given-name"
-              className="block w-full rounded-md bg-transparent px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
+              className="block w-full rounded-md border bg-transparent px-3.5 py-2 text-base text-gray-900 placeholder:text-gray-400 focus:border-none focus:ring-2 focus:ring-sky-400"
             />
           </div>
         </div>
@@ -34,24 +34,7 @@ const ContactForm = () => {
               name="last-name"
               type="text"
               autoComplete="family-name"
-              className="block w-full rounded-md bg-transparent px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
-            />
-          </div>
-        </div>
-        <div className="sm:col-span-2">
-          <label
-            htmlFor="company"
-            className="block text-sm/6 font-semibold text-gray-900"
-          >
-            Company
-          </label>
-          <div className="mt-2.5">
-            <input
-              id="company"
-              name="company"
-              type="text"
-              autoComplete="organization"
-              className="block w-full rounded-md bg-transparent px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
+              className="block w-full rounded-md border bg-transparent px-3.5 py-2 text-base text-gray-900 placeholder:text-gray-400 focus:border-none focus:ring-2 focus:ring-sky-400"
             />
           </div>
         </div>
@@ -68,7 +51,7 @@ const ContactForm = () => {
               name="email"
               type="email"
               autoComplete="email"
-              className="block w-full rounded-md bg-transparent px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
+              className="block w-full rounded-md border bg-transparent px-3.5 py-2 text-base text-gray-900 placeholder:text-gray-400 focus:border-none focus:ring-2 focus:ring-sky-400"
             />
           </div>
         </div>
@@ -87,7 +70,7 @@ const ContactForm = () => {
                   name="country"
                   autoComplete="country"
                   aria-label="Country"
-                  className="col-start-1 row-start-1 w-full appearance-none rounded-md bg-transparent py-2 pl-3.5 pr-7 text-base text-gray-500 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                  className="col-start-1 row-start-1 w-full appearance-none rounded-md border bg-transparent py-2 pl-3.5 pr-7 text-base text-gray-500 placeholder:text-gray-400 focus:border-none focus:ring-2 focus:ring-sky-400"
                 >
                   <option>US</option>
                   <option>CA</option>
@@ -99,7 +82,7 @@ const ContactForm = () => {
                 name="phone-number"
                 type="text"
                 placeholder="123-456-7890"
-                className="block min-w-0 grow bg-transparent py-1.5 pl-1 pr-3 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6"
+                className="block min-w-0 grow rounded-md bg-transparent py-1.5 pl-1 pr-3 text-base text-gray-900 placeholder:text-gray-400 focus:border-none focus:outline-none focus:ring-2 focus:ring-sky-400 sm:text-sm/6"
               />
             </div>
           </div>
@@ -116,7 +99,7 @@ const ContactForm = () => {
               id="message"
               name="message"
               rows={4}
-              className="block w-full rounded-md bg-transparent px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
+              className="block w-full rounded-md border bg-transparent px-3.5 py-2 text-base text-gray-900 outline-gray-300 placeholder:text-gray-400 focus:border-none focus:ring-2 focus:ring-sky-400"
               defaultValue={''}
             />
           </div>
@@ -151,10 +134,10 @@ const ContactForm = () => {
       </div>
       <div className="mt-10">
         <button
-          type="submit"
-          className="shadow-xs block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          type="button"
+          className="shadow-xs block w-full rounded-md bg-sky-400 px-3.5 py-2.5 text-center text-sm font-semibold text-white hover:bg-sky-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
         >
-          Let's talk
+          Let&apos;s talk
         </button>
       </div>
     </form>
