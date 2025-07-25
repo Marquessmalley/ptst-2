4deedb52cac0c8fadb36642710aec6faa370d6b3
msmalley-bizstream <msmalley@bizstream.com>
2025-07-25 11:46:02 -0400

    Added metadata to page

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,29 @@
+import { type Metadata } from 'next';
 import { Hero, Pricing, Gallery, Faq, Contact } from '@/components/sections';
+
+export const metadata: Metadata = {
+  title: 'Paul & Tev Shine Time',
+  description:
+    'All-purpose mobile cleaning service specializing in quality interior and exterior car detailing in the West Michigan area. Family-owned business in memory of Paul Williams.',
+  keywords: [
+    'car wash',
+    'car detailing',
+    'auto detailing',
+    'mobile detailing',
+    'interior detail',
+    'exterior detail',
+    'full detail',
+    'vehicle cleaning',
+    'ptshinetime',
+    'paul & tev shine time',
+  ],
+  authors: [
+    { name: 'Tevin Williams' },
+    { name: 'Marques Smalley', url: 'https://marquessmalley.netlify.app/' },
+  ],
+  creator: 'Marques Smalley',
+  publisher: 'Tevin Williams',
+};
 
 export default function Page() {
   return (
