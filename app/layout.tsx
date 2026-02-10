34a2ed0b5c6639e1f8985aa709aa2ee9a8111e0e
Marques Smalley <marquessmalley@gmail.com>
2026-02-09 20:25:54 -0500

    Refactor Prettier configuration to remove singleQuote and printWidth settings. Standardize quotation marks in layout.tsx for improved consistency and readability.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,10 @@
-'use client';
-import '@/components/ui/global.css';
-import { HeroUIProvider } from '@heroui/react';
-import SplashScreen from '@/components/ui/splashscreen/SplashScreen';
-import { useState, useEffect } from 'react';
-import Menu from '@/components/ui/navbar/Menu';
-import Footer from '@/components/ui/footer';
+"use client";
+import "@/components/ui/global.css";
+import { HeroUIProvider } from "@heroui/react";
+import SplashScreen from "@/components/ui/splashscreen/SplashScreen";
+import { useState, useEffect } from "react";
+import Menu from "@/components/ui/navbar/Menu";
+import Footer from "@/components/ui/footer";
 
 export default function RootLayout({
   children,
@@ -21,17 +21,17 @@ export default function RootLayout({
     return () => clearTimeout(timer);
   }, []);
   return (
-    <html lang='en'>
+    <html lang="en">
       <head>
-        <link rel='preconnect' href='https://fonts.googleapis.com' />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
-          rel='preconnect'
-          href='https://fonts.gstatic.com'
-          crossOrigin='anonymous'
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
         />
         <link
-          href='https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&display=swap'
-          rel='stylesheet'
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&display=swap"
+          rel="stylesheet"
         />
       </head>
       <body className="flex min-h-screen flex-col font-['Inter',sans-serif]">
@@ -39,9 +39,9 @@ export default function RootLayout({
           {isLoading ? (
             <SplashScreen />
           ) : (
-            <div className='flex min-h-screen flex-col bg-gradient-to-br from-sky-300/40 via-slate-50 to-orange-300/40'>
+            <div className="flex min-h-screen flex-col bg-gradient-to-br from-sky-300/40 via-slate-50 to-orange-300/40">
               <Menu />
-              <main className='animate__animated animate__fadeIn animate__slow flex flex-grow items-center justify-center'>
+              <main className="animate__animated animate__fadeIn animate__slow flex flex-grow items-center justify-center">
                 {children}
               </main>
               <Footer />
