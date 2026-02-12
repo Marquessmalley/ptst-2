bf6fa7daa077b7e1549c0e39da38c54f0555bfb3
Marques Smalley <marquessmalley@gmail.com>
2026-02-11 20:15:05 -0500

    Update package-lock.json for dependency version upgrades and rename project to 'ptst-2'. Modify tailwind.config.ts to include additional path for theme files.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,7 @@ const config: Config = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/**/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
